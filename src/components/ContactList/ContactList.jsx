767fb90ea03dc2cf1d2b01a3c8c94cbcbbc11f2d
Artem <artembariga5@gmail.com>
2023-07-29 18:37:44 +0300

    update selectors

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,19 +1,15 @@
 import css from './ContactsList.module.css';
 import { useSelector } from 'react-redux';
-import { selectContacts, selectFilters } from 'redux/selectors';
+import { selectVisibleContacts } from 'redux/selectors';
 import { ContactItem } from 'components/ContactItem/ContactItem';
 
 export const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const filters = useSelector(selectFilters);
-  const visibleContacts = () => {
-    return contacts.filter(contact => contact.name.includes(filters));
-  };
+  const visibleContacts = useSelector(selectVisibleContacts);
 
   return (
     <div className={css.container}>
       <ul className={css.list}>
-        {visibleContacts().map(({ id, name, phone }) => {
+        {visibleContacts.map(({ id, name, phone }) => {
           return <ContactItem id={id} name={name} phone={phone} key={id} />;
         })}
       </ul>
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContacts = state => state.contacts.items;
 
 export const selectFilters = state => state.filters.status;
@@ -5,3 +7,10 @@ export const selectFilters = state => state.filters.status;
 export const selectIsLoading = state => state.contacts.isLoading;
 
 export const selectError = state => state.contacts.erorr;
+
+export const selectVisibleContacts = createSelector(
+  [selectContacts, selectFilters],
+  (contacts, filters) => {
+    return contacts.filter(contact => contact.name.includes(filters));
+  }
+);
